8bee1a95c1b5dcf4247f6bf6fe54fa07dfcad295
nguyenanhvu <nganhvu.gon@gmail.com>
2016-09-12 19:57:46 +0700

    Create Project

---
+++ FriZo/Session.swift
@@ -0,0 +1,13 @@
+//
+//  Session.swift
+//  FriZo
+//
+//  Created by Vu Nguyen on 9/12/16.
+//  Copyright © 2016 FriZo. All rights reserved.
+//
+
+import Foundation
+
+class Session: NSObject {
+    
+}
---
+++ FriZo/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  FriZo
+//
+//  Created by Vu Nguyen on 9/12/16.
+//  Copyright © 2016 FriZo. All rights reserved.
+//
+
+import Foundation
+
+class User: NSObject {
+    
+}
