12ab6a3f66c435718f4aa672ae3dc64ece6b9195
nguyenanhvu <nganhvu.gon@gmail.com>
2016-09-21 23:46:51 +0700

    Fix extendable

---
+++ FriZo/ChatViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChatViewController.swift
+//  FriZo
+//
+//  Created by Vu Nguyen on 9/21/16.
+//  Copyright © 2016 FriZo. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    
+    @IBOutlet var chatTextView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+
+}
