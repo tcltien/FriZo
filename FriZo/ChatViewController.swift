e37708e4a81a6d83cfa5c44c6a2b55de94e9b54c
nguyenanhvu <nganhvu.gon@gmail.com>
2016-09-23 23:32:26 +0700

    auto layout text field with

--- FriZo/ChatViewController.swift
+++ FriZo/ChatViewController.swift
@@ -11,14 +11,53 @@ import UIKit
 class ChatViewController: UIViewController {
     
     
+    @IBOutlet weak var bottomHeight: NSLayoutConstraint!
     @IBOutlet var chatTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+    }
+    
+
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShowNotification(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHideNotification(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+  
+    }
+    
+    
+    func keyboardWillShowNotification(notification: Notification) {
+        if let userInfor = notification.userInfo{
+        if let keyboardSize = (userInfor[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            bottomHeight.constant = keyboardSize.height
+            } }
+        view.setNeedsLayout()
+    }
+    
+    func keyboardWillHideNotification(notification: Notification) {
+        bottomHeight.constant = 0
+        view.setNeedsLayout()
     }
 
-  
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        
+        view.endEditing(true)
+    }
+
 
 }
