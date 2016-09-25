e4ab3339085c1f08dd5d44e720f0d182ca882393
nguyenanhvu <nganhvu.gon@gmail.com>
2016-09-25 11:19:35 +0700

    Implement Circular Profile Picture

---
+++ FriZo/UserInformationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UserInformationViewController.swift
+//  FriZo
+//
+//  Created by Vu Nguyen on 9/24/16.
+//  Copyright © 2016 FriZo. All rights reserved.
+//
+
+import UIKit
+
+class UserInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FriZo/UserPicCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserPicCell.swift
+//  FriZo
+//
+//  Created by Vu Nguyen on 9/24/16.
+//  Copyright © 2016 FriZo. All rights reserved.
+//
+
+import UIKit
+
+class UserPicCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
